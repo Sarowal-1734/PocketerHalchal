122a54ddf8477e0bbd7345daca173edaf6b233cb
Sarowal-1734 <sarowalhossain1997@gmail.com>
2020-10-24 10:03:06 +0600

    Design Phase 4 (Monthly Report 1.2)

--- app/src/main/java/com/dynamic_host/pocketerhalchal/MainActivity.java
+++ app/src/main/java/com/dynamic_host/pocketerhalchal/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         btReport.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityMonthlyReport.class);
+                Intent intent = new Intent(MainActivity.this, Activity_Report.class);
                 startActivity(intent);
             }
         });

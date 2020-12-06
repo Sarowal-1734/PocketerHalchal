b0ccde97ef4943b0c28659c029f67e0571ac2422
Soyaeeb <monirul15-1742@diu.edu.bd>
2020-12-06 22:51:13 +0600

    Add Swipe option on ListView Item for edit and delete

--- app/src/main/java/com/dynamic_host/pocketerhalchal/ReportActivity.java
+++ app/src/main/java/com/dynamic_host/pocketerhalchal/ReportActivity.java
@@ -1,27 +1,31 @@
 package com.dynamic_host.pocketerhalchal;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.ContentUris;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CheckBox;
-import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
 import com.baoyz.swipemenulistview.SwipeMenuListView;
-import com.dynamic_host.pocketerhalchal.database.PocketContract;
-import com.dynamic_host.pocketerhalchal.database.PocketContract.IncomeEntry;
 import com.dynamic_host.pocketerhalchal.database.PocketContract.ExpenseEntry;
+import com.dynamic_host.pocketerhalchal.database.PocketContract.IncomeEntry;
 import com.dynamic_host.pocketerhalchal.database.PocketCursorAdapter;
 import com.dynamic_host.pocketerhalchal.database.SharedPreference;
 
@@ -29,7 +33,7 @@ public class ReportActivity extends AppCompatActivity{
     CheckBox cbIncome;
     CheckBox cbExpense;
     SwipeMenuListView listView;
-    String source, incomeDescription, expenseDescription, item;
+    String source, incomeDescription, expenseDescription, incomeDate, expenseDate, item;
     Uri uri;
 
     @Override
@@ -45,78 +49,123 @@ public class ReportActivity extends AppCompatActivity{
         cbExpense = findViewById(R.id.cbExpense);
         listView = (SwipeMenuListView) findViewById(R.id.list_report);
 
+        //Creating swipe option on listview item for delete a edit
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+            @Override
+            public void create(SwipeMenu menu) {
+                // Create edit item
+                SwipeMenuItem editItem = new SwipeMenuItem(getApplicationContext());
+                editItem.setBackground(new ColorDrawable(Color.rgb(32,98,35)));
+                editItem.setWidth(150);
+                editItem.setIcon(R.drawable.ic_edit_item);
+                menu.addMenuItem(editItem);
+                // create delete item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(getApplicationContext());
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,0x3F,25)));
+                deleteItem.setWidth(150);
+                deleteItem.setIcon(R.drawable.ic_delete_item);
+                menu.addMenuItem(deleteItem);
+            }
+        };
+        listView.setMenuCreator(creator);
+        //OnClick listner for listview Item in Swipe option
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                int  id = (int) listView.getItemIdAtPosition(position);
+                switch (index){
+                    case 0:
+                        if (cbIncome.isChecked()){
+                            Intent intent = new Intent(ReportActivity.this,IncomeEditorActivity.class);
+                            Uri uri = ContentUris.withAppendedId(IncomeEntry.CONTENT_INCOME_URI,id);
+                            intent.setData(uri);
+                            startActivity(intent);
+                        }
+                        else {
+                            Intent intent = new Intent(ReportActivity.this,ExpenseEditorActivity.class);
+                            Uri uri = ContentUris.withAppendedId(ExpenseEntry.CONTENT_EXPENSE_URI,id);
+                            intent.setData(uri);
+                            startActivity(intent);
+                        }
+                        break;
+                    case 1:
+                        AlertDialog.Builder ab = new AlertDialog.Builder(ReportActivity.this);
+                        ab.setTitle("Are you sure to delete?");
+                        //Setting positive "Ok" Button
+                        ab.setPositiveButton("Yes",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int which) {
+                                        if(cbIncome.isChecked()){
+                                            uri = ContentUris.withAppendedId(IncomeEntry.CONTENT_INCOME_URI,position+1);
+                                            getContentResolver().delete(uri,null,null);
+                                            display();
+                                        }
+                                        else {
+                                            uri = ContentUris.withAppendedId(ExpenseEntry.CONTENT_EXPENSE_URI,position+1);
+                                            getContentResolver().delete(uri,null,null);
+                                            display();
+                                        }
+                                    }
+                                });
+                        //Setting Negative "Cancel" Button
+                        ab.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                               dialog.cancel();
+                            }
+                        });
+                        ab.show();
+                        break;
+                }
+                return false;
+            }
+        });
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                // To show the description of the item
                 AlertDialog.Builder ab = new AlertDialog.Builder(ReportActivity.this);
-                ab.setTitle("Delete Or Edit Item");
+                ab.setTitle("Item Information");
 
                 if(cbIncome.isChecked()){
                     uri = ContentUris.withAppendedId(IncomeEntry.CONTENT_INCOME_URI,id);
                     String[] incomeProjection = {
                             IncomeEntry.COLUMN_INCOME_SOURCE,
-                            IncomeEntry.COLUMN_INCOME_DESCRIPTION
+                            IncomeEntry.COLUMN_INCOME_DESCRIPTION,
+                            IncomeEntry.COLUMN_INCOME_DATE
                     };
                     Cursor cursor = getContentResolver().query(uri,incomeProjection,null,null,null);
-                    int sourceColumnIndex = cursor.getColumnIndex(IncomeEntry.COLUMN_INCOME_SOURCE);
-                    int descriptionColumnIndex = cursor.getColumnIndex(IncomeEntry.COLUMN_INCOME_DESCRIPTION);
                     if(cursor.moveToNext())
                     {
-                        source = cursor.getString(sourceColumnIndex);
-                        incomeDescription = cursor.getString(descriptionColumnIndex);
+                        source = cursor.getString(cursor.getColumnIndex(IncomeEntry.COLUMN_INCOME_SOURCE));
+                        incomeDescription = cursor.getString(cursor.getColumnIndex(IncomeEntry.COLUMN_INCOME_DESCRIPTION));
+                        incomeDate = cursor.getString(cursor.getColumnIndex(IncomeEntry.COLUMN_INCOME_DATE));
                     }
                     if (TextUtils.isEmpty(incomeDescription))
-                        ab.setMessage("Source: "+source);
+                        ab.setMessage("Source: "+source+"\n\n"+incomeDate);
                     else
-                        ab.setMessage("Source: "+source+"\nDescription: "+incomeDescription);
+                        ab.setMessage("Source: "+source+"\nDescription: "+incomeDescription+"\n\n"+incomeDate);
 
                 }
                 else {
                     uri = ContentUris.withAppendedId(ExpenseEntry.CONTENT_EXPENSE_URI,id);
                     String[] expenseProjection = {
                             ExpenseEntry.COLUMN_EXPENSE_ITEM,
-                            ExpenseEntry.COLUMN_EXPENSE_DESCRIPTION
+                            ExpenseEntry.COLUMN_EXPENSE_DESCRIPTION,
+                            ExpenseEntry.COLUMN_EXPENSE_DATE
                     };
                     Cursor cursor = getContentResolver().query(uri,expenseProjection,null,null,null);
-                    int itemColumnIndex = cursor.getColumnIndex(ExpenseEntry.COLUMN_EXPENSE_ITEM);
-                    int descriptionColumnIndex = cursor.getColumnIndex(ExpenseEntry.COLUMN_EXPENSE_DESCRIPTION);
                     if(cursor.moveToNext())
                     {
-                        item = cursor.getString(itemColumnIndex);
-                        expenseDescription = cursor.getString(descriptionColumnIndex);
+                        item = cursor.getString(cursor.getColumnIndex(ExpenseEntry.COLUMN_EXPENSE_ITEM));
+                        expenseDescription = cursor.getString(cursor.getColumnIndex(ExpenseEntry.COLUMN_EXPENSE_DESCRIPTION));
+                        expenseDate = cursor.getString(cursor.getColumnIndex(ExpenseEntry.COLUMN_EXPENSE_DATE));
                     }
                     if (TextUtils.isEmpty(expenseDescription))
-                        ab.setMessage("Item: "+item);
+                        ab.setMessage("Item: "+item+"\n\n"+expenseDate);
                     else
-                        ab.setMessage("Item: "+item+"\nDescription: "+expenseDescription);
+                        ab.setMessage("Item: "+item+"\nDescription: "+expenseDescription+"\n\n"+expenseDate);
                 }
-
-                //Setting positive "Save" Button
-                ab.setPositiveButton("Edit",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int which) {
-                                if (cbIncome.isChecked()){
-                                    Intent intent = new Intent(ReportActivity.this,IncomeEditorActivity.class);
-                                    Uri uri = ContentUris.withAppendedId(IncomeEntry.CONTENT_INCOME_URI,id);
-                                    intent.setData(uri);
-                                    startActivity(intent);
-                                }
-                                else {
-                                    Intent intent = new Intent(ReportActivity.this,ExpenseEditorActivity.class);
-                                    Uri uri = ContentUris.withAppendedId(ExpenseEntry.CONTENT_EXPENSE_URI,id);
-                                    intent.setData(uri);
-                                    startActivity(intent);
-                                }
-                            }
-                        });
-                //Setting Negative "Cancel" Button
-                ab.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        getContentResolver().delete(uri,null,null);
-                        display();
-                    }
-                });
                 ab.show();
             }
         });
@@ -130,6 +179,7 @@ public class ReportActivity extends AppCompatActivity{
         }
     }
 
+    // Display Item on List view
     private void display() {
         if(cbIncome.isChecked()){
             String[] incomeProjection = {IncomeEntry.INCOME_ID,
